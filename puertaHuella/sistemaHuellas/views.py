77e471f2050ec2dac2dd8990e4ee00bee20a0a97
sergio <pepe123@hotmail.com>
2018-11-05 14:26:45 -0300

    1.2

--- puertaHuella/sistemaHuellas/views.py
+++ puertaHuella/sistemaHuellas/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from puertaHuella import settings 
 from .models import Alumno, Ingreso
 from .models import Docente
+import random
 # Create your vie.ws here.
 
 def visor(request):
@@ -12,11 +13,22 @@ def visor(request):
     persona=None
     if ingreso:
         persona = Alumno.objects.filter(numeroRegistro=ingreso.numRegistro).first()
-    if persona:
-        bienvenido = "Alumno"
+        ingreso.activo = False
+        ingreso.save()
+        if persona:
+            bienvenido = "Alumno"
+        else:
+            persona = Docente.objects.filter(numeroRegistro=ingreso.numRegistro).first()
+            bienvenido = "Docente"
+
+        src = '{}{}.{}'.format('\\media\\images\\', persona.numeroRegistro, 'jpg')
+        return render(request,'bienvenidoPersona.html',{'nombre':persona.nombre,'apellido':persona.apellido, 
+                'src':src, 'bienvenido': bienvenido})
+        
     else:
-        persona = Docente.objects.filter(numeroRegistro=ingreso.numRegistro).first()
-        bienvenido = "Docente"
+        src = '{}{}.{}'.format('\\media\\images\\', "default_user", 'jpg')
+        return render(request,'welcome.html',{'src':src})
+        
 
     #alumnos = Alumno.objects.filter(dni=32434)
     #docente =  Docente.objects.filter(dni=1234)
@@ -26,8 +38,7 @@ def visor(request):
     #docente = docente[0]
     #DETERMINAR SI ES ALUMNO O DOCENTE PARA MOSTRAR UNA U OTRA VISTA. HECHO
     #COLOCAR DIRECCION DE LOS RECURSOS COMO CSS U IMAGENES, PARA QUE NO TE APARESCA COMO LINK
-    src = '{}{}.{}'.format('\\media\\images\\', persona.numeroRegistro, 'jpg')
+    
 
     
-    return render(request,'bienvenidoPersona.html',{'nombre':persona.nombre,'apellido':persona.apellido, 
-                'src':src, 'bienvenido': bienvenido})
+    
